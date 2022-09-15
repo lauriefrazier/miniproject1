540facc54509eb2fa698ca3f768c45bfaf93b764
Laurie Frazier <l_frazier3@mail.fhsu.edu>
2022-09-15 17:43:11 -0600

    Gathered Apple stock prices for AAPL.

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 #Laurie Frazier
 #Mini Project 1
 
+import yfinance as yf
+
+data = yf.download("AAPL", start="2022-08-30", end="2022-09-14")
+
+aaplPrices = []
+
+print(data["Adj Close"])
+
+for price in data["Adj Close"]:
+    aaplPrices.append(price)
+
+print(aaplPrices)
+
