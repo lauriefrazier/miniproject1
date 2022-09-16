3b312908e23c9340696f54a75c10e3297c14a452
Laurie Frazier <l_frazier3@mail.fhsu.edu>
2022-09-15 18:04:19 -0600

    Added AAPL as an export PNG file.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 #Mini Project 1
 
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 data = yf.download("AAPL", start="2022-08-30", end="2022-09-14")
 
@@ -14,4 +17,15 @@ for price in data["Adj Close"]:
     aaplPrices.append(price)
 
 print(aaplPrices)
+
+#Create numpy array
+aaplarray = np.array(aaplPrices)
+
+#Create matplotlib graph
+plt.plot(aaplarray)
+
+#Show the graph
+plt.savefig("Charts/aapl.png")
+plt.show()
+
 
